66ff69de644a06c7ef8cfd3d506baffbc761cf70
Dunois <tarry403@gmail.com>
2016-11-01 20:23:30 +0900

    AnimationCheckerProUpdater
    Ver 1.4

---
+++ ACPUpdater/MainForm.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.SelfUpdateButton = New System.Windows.Forms.Button()
+        Me.FormCloseButton = New System.Windows.Forms.Button()
+        Me.DownloadProgressBar = New System.Windows.Forms.ProgressBar()
+        Me.SpeedLabel = New System.Windows.Forms.Label()
+        Me.UpdateInformationLabel = New System.Windows.Forms.Label()
+        Me.UpdaterVersionLabel = New System.Windows.Forms.Label()
+        Me.CurrentDownloadLabel = New System.Windows.Forms.Label()
+        Me.AnimationTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'SelfUpdateButton
+        '
+        Me.SelfUpdateButton.Location = New System.Drawing.Point(529, 12)
+        Me.SelfUpdateButton.Name = "SelfUpdateButton"
+        Me.SelfUpdateButton.Size = New System.Drawing.Size(109, 23)
+        Me.SelfUpdateButton.TabIndex = 1
+        Me.SelfUpdateButton.Text = "수동 업데이트"
+        Me.SelfUpdateButton.UseVisualStyleBackColor = True
+        '
+        'FormCloseButton
+        '
+        Me.FormCloseButton.Location = New System.Drawing.Point(563, 70)
+        Me.FormCloseButton.Name = "FormCloseButton"
+        Me.FormCloseButton.Size = New System.Drawing.Size(75, 23)
+        Me.FormCloseButton.TabIndex = 2
+        Me.FormCloseButton.Text = "닫기"
+        Me.FormCloseButton.UseVisualStyleBackColor = True
+        '
+        'DownloadProgressBar
+        '
+        Me.DownloadProgressBar.Location = New System.Drawing.Point(12, 41)
+        Me.DownloadProgressBar.Name = "DownloadProgressBar"
+        Me.DownloadProgressBar.Size = New System.Drawing.Size(626, 23)
+        Me.DownloadProgressBar.TabIndex = 3
+        '
+        'SpeedLabel
+        '
+        Me.SpeedLabel.AutoSize = True
+        Me.SpeedLabel.Location = New System.Drawing.Point(12, 16)
+        Me.SpeedLabel.Name = "SpeedLabel"
+        Me.SpeedLabel.Size = New System.Drawing.Size(31, 15)
+        Me.SpeedLabel.TabIndex = 4
+        Me.SpeedLabel.Text = "속도"
+        '
+        'UpdateInformationLabel
+        '
+        Me.UpdateInformationLabel.AutoSize = True
+        Me.UpdateInformationLabel.Location = New System.Drawing.Point(12, 74)
+        Me.UpdateInformationLabel.Name = "UpdateInformationLabel"
+        Me.UpdateInformationLabel.Size = New System.Drawing.Size(83, 15)
+        Me.UpdateInformationLabel.TabIndex = 5
+        Me.UpdateInformationLabel.Text = "업데이트 정보"
+        '
+        'UpdaterVersionLabel
+        '
+        Me.UpdaterVersionLabel.AutoSize = True
+        Me.UpdaterVersionLabel.Location = New System.Drawing.Point(401, 16)
+        Me.UpdaterVersionLabel.Name = "UpdaterVersionLabel"
+        Me.UpdaterVersionLabel.Size = New System.Drawing.Size(95, 15)
+        Me.UpdaterVersionLabel.TabIndex = 6
+        Me.UpdaterVersionLabel.Text = "업데이터 버전 : "
+        '
+        'CurrentDownloadLabel
+        '
+        Me.CurrentDownloadLabel.AutoSize = True
+        Me.CurrentDownloadLabel.Location = New System.Drawing.Point(152, 16)
+        Me.CurrentDownloadLabel.Name = "CurrentDownloadLabel"
+        Me.CurrentDownloadLabel.Size = New System.Drawing.Size(59, 15)
+        Me.CurrentDownloadLabel.TabIndex = 7
+        Me.CurrentDownloadLabel.Text = "전체 상황"
+        '
+        'AnimationTimer
+        '
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(650, 104)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.CurrentDownloadLabel)
+        Me.Controls.Add(Me.UpdaterVersionLabel)
+        Me.Controls.Add(Me.UpdateInformationLabel)
+        Me.Controls.Add(Me.SpeedLabel)
+        Me.Controls.Add(Me.DownloadProgressBar)
+        Me.Controls.Add(Me.FormCloseButton)
+        Me.Controls.Add(Me.SelfUpdateButton)
+        Me.Font = New System.Drawing.Font("나눔고딕", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "AnimationCheckerPro Updater"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SelfUpdateButton As Button
+    Friend WithEvents FormCloseButton As Button
+    Friend WithEvents DownloadProgressBar As ProgressBar
+    Friend WithEvents SpeedLabel As Label
+    Friend WithEvents UpdateInformationLabel As Label
+    Friend WithEvents UpdaterVersionLabel As Label
+    Friend WithEvents CurrentDownloadLabel As Label
+    Friend WithEvents AnimationTimer As Timer
+End Class
---
+++ ACPUpdater/MainForm.vb
@@ -0,0 +1,163 @@
+﻿Imports System.Environment
+Imports System.IO
+Imports System.Net
+
+Public Class MainForm
+    Public Version As Double = 1.4
+    Public Declare Function GetPrivateProfileString Lib "kernel32" Alias "GetPrivateProfileStringA" (ByVal lpApplicationName As String, ByVal lpKeyName As String, ByVal lpDefault As String, ByVal lpReturnedString As String, ByVal nSize As Integer, ByVal lpFileName As String) As Integer
+    Dim InformationFolder As String
+    Dim UpdateLocation As String
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        UpdaterVersionLabel.Text = UpdaterVersionLabel.Text & Version
+        SpeedLabel.Visible = False
+        UpdateInformationLabel.Visible = False
+        CurrentDownloadLabel.Visible = False
+        AnimationTimer.Enabled = True
+        If My.Computer.FileSystem.DirectoryExists(GetFolderPath(SpecialFolder.ApplicationData) & "\Dunois Software\AnimationCheckerPro") Then
+            InformationFolder = GetFolderPath(SpecialFolder.ApplicationData) & "\Dunois Software\AnimationCheckerPro"
+            UpdateLocation = XMLReader(InformationFolder & "\Settings.xml", "System", "ProgramPath")
+            XMLWriter(InformationFolder & "\Settings.xml", "System", "UpdaterVersion", Version)
+        Else
+            MsgBox("잘못된 접근입니다.", MsgBoxStyle.OkOnly + MsgBoxStyle.Critical, "오류")
+        End If
+    End Sub
+    Function UpdateCheck()
+        If My.Computer.FileSystem.FileExists(InformationFolder & "\Settings.xml") Then
+            Dim UpdateStatus As Integer = XMLReader(InformationFolder & "\Settings.xml", "System", "UpdateStatus")
+            If UpdateStatus = 1 Then
+                Return 0
+            Else
+                Return 1
+            End If
+        Else
+            Return 1
+        End If
+    End Function
+    Public Sub ACPKill()
+        For Each prog As Process In Process.GetProcesses
+            If prog.ProcessName = "AnimationCheckerPro" Then
+                prog.Kill()
+            End If
+        Next
+    End Sub
+    Function XMLReader(ByVal XMLPath As String, ByVal Element_1 As String, ByVal Element_2 As String)
+        Dim xml = XDocument.Load(XMLPath)
+        Try
+            Return xml.Element(Element_1).Element(Element_2).Value
+        Catch ex As Exception
+            Return "Error"
+        End Try
+    End Function
+    Public Sub XMLWriter(ByVal XMLFile As String, ByVal Element_1 As String, ByVal Element_2 As String, ByVal Value As String)
+        Dim xmle As XElement = XElement.Load(XMLFile)
+        If xmle.Element(Element_2) Is Nothing Then
+            xmle.Add(New XElement(Element_2, Value))
+            xmle.Save(XMLFile)
+        Else
+            xmle.SetElementValue(Element_2, Value)
+            xmle.Save(XMLFile)
+        End If
+    End Sub
+    Public Sub GetFileFromUrl(ByVal url As String, ByVal DownloadDestination As String)
+        FormCloseButton.Enabled = False
+        Try
+            Dim ReDownBufferSize As Double = 0
+            Dim ThisRequest As HttpWebRequest = DirectCast(WebRequest.Create(url), HttpWebRequest)
+            Dim ThisResponese As HttpWebResponse = DirectCast(ThisRequest.GetResponse(), HttpWebResponse)
+            Dim TotalSize As Integer = ThisResponese.ContentLength
+            Dim MyThisStream As Stream = ThisResponese.GetResponseStream()
+            Dim GetFileStream As New FileStream(DownloadDestination, FileMode.Create)
+            Dim buff As Byte() = New Byte(TotalSize) {}
+            Dim buffer As Byte() = buff
+            Dim TMP As Long
+            Dim StartTime As DateTime = Now
+            Try
+                Do Until GetFileStream.Length = TotalSize
+                    ReDownBufferSize = MyThisStream.Read(buff, 0, 1024)
+                    GetFileStream.Write(buffer, 0, ReDownBufferSize)
+
+                    TMP = DateDiff("s", StartTime, Now) + 1
+                    Application.DoEvents()
+                    DownloadProgressBar.Value = Math.Round(GetFileStream.Length) / Math.Round(TotalSize) * 100
+                    CurrentDownloadLabel.Text = Math.Round(GetFileStream.Length / 1024 ^ 2, 2) & "MB / " & Math.Round(TotalSize / 1024 ^ 2, 2) & "MB)"
+                    If (GetFileStream.Length / TMP / 1024) > 850 Then
+                        SpeedLabel.Text = Format(((GetFileStream.Length / TMP / 1024) / 1024), "#0.##") & " MB/sec"
+                    Else
+                        SpeedLabel.Text = Format(((GetFileStream.Length / TMP / 1024) / 1024), "#0.##") & " KB/sec"
+                    End If
+                Loop
+            Catch ex As Exception
+                End
+            End Try
+            GetFileStream.Close()
+            MyThisStream.Close()
+            ThisResponese.Close()
+            DownloadProgressBar.Value = 100
+        Catch ex As Exception
+            MsgBox("다운로드에 실패하였습니다.", MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "오류")
+        End Try
+    End Sub
+
+    Private Sub FormCloseButton_Click(sender As Object, e As EventArgs) Handles FormCloseButton.Click
+        Close()
+    End Sub
+
+    Private Sub AnimationTimer_Tick(sender As Object, e As EventArgs) Handles AnimationTimer.Tick
+        AnimationTimer.Enabled = False
+        If UpdateCheck() = 0 Then
+            ACPKill()
+            SpeedLabel.Visible = True
+            UpdateInformationLabel.Visible = True
+            CurrentDownloadLabel.Visible = True
+            ACPDelete(UpdateLocation)
+            DownloadVerInfo()
+            Dim OldVersion As Double = XMLReader(InformationFolder & "\Settings.xml", "System", "ProgramVersion")
+            Dim NewVersion As Double = INIRead("System", "Version", My.Computer.FileSystem.SpecialDirectories.CurrentUserApplicationData & "\ProgramVersion.ini")
+            UpdateInformationLabel.Text = OldVersion & "버전 에서 " & NewVersion & "버전 으로 업데이트 합니다."
+            GetFileFromUrl("http://dunois403.cafe24.com/ACPData/AnimationCheckerPro_Temp.exe", UpdateLocation & "\AnimationCheckerPro.exe")
+            Shell(UpdateLocation & "\AnimationCheckerPro.exe", AppWinStyle.NormalFocus)
+            Close()
+        Else
+            MsgBox("잘못된 접근입니다.", MsgBoxStyle.OkOnly + MsgBoxStyle.Critical, "오류")
+        End If
+    End Sub
+    Public Sub DownloadVerInfo()
+        If My.Computer.FileSystem.FileExists(My.Computer.FileSystem.SpecialDirectories.CurrentUserApplicationData & "\ProgramVersion.ini") Then
+            My.Computer.FileSystem.DeleteFile(My.Computer.FileSystem.SpecialDirectories.CurrentUserApplicationData & "\ProgramVersion.ini")
+        End If
+        My.Computer.Network.DownloadFile("http://dunois403.cafe24.com/ACPData/ACPVersion.ini", My.Computer.FileSystem.SpecialDirectories.CurrentUserApplicationData & "\ProgramVersion.ini")
+    End Sub
+    Public Sub ACPDelete(ByVal ProgramLocation As String)
+        If My.Computer.FileSystem.FileExists(ProgramLocation & "\AnimationCheckerPro.exe") Then
+            My.Computer.FileSystem.DeleteFile(ProgramLocation & "\AnimationCheckerPro.exe")
+        End If
+    End Sub
+    Public Function INIRead(ByVal Session As String, ByVal KeyValue As String, ByVal INIFILE As String) As String
+        Dim s As New String("", 1024)
+        Dim ReturnValue As Long
+        ReturnValue = GetPrivateProfileString(Session, KeyValue, "", s, 1024, INIFILE)
+        Return Mid(s, 1, InStr(s, Chr(0)) - 1)
+
+    End Function
+
+    Private Sub SelfUpdateButton_Click(sender As Object, e As EventArgs) Handles SelfUpdateButton.Click
+        Dim SelectedFolder As String
+        Dim OpenFolderDialog As New FolderBrowserDialog
+        OpenFolderDialog.ShowNewFolderButton = True
+        OpenFolderDialog.Description = "다운로드 폴더 선택"
+        If (OpenFolderDialog.ShowDialog() = DialogResult.OK) Then
+            SelectedFolder = OpenFolderDialog.SelectedPath
+            ACPKill()
+            SpeedLabel.Visible = True
+            UpdateInformationLabel.Visible = True
+            CurrentDownloadLabel.Visible = True
+            ACPDelete(SelectedFolder)
+            DownloadVerInfo()
+            Dim NewVersion As Double = INIRead("System", "Version", My.Computer.FileSystem.SpecialDirectories.CurrentUserApplicationData & "\ProgramVersion.ini")
+            UpdateInformationLabel.Text = NewVersion & "버전을 다운로드 합니다."
+            GetFileFromUrl("http://dunois403.cafe24.com/ACPData/AnimationCheckerPro_Temp.exe", UpdateLocation & "\AnimationCheckerPro.exe")
+            Shell(UpdateLocation & "\AnimationCheckerPro.exe", AppWinStyle.NormalFocus)
+            Close()
+        End If
+    End Sub
+End Class
